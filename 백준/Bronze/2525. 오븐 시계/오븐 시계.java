e2713714ea260c55f0ef12be637cb3d9f14b74d7
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2023-02-19 14:34:39 +0900

    Update 오븐 시계.java

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -5,42 +5,18 @@ public class Main {
 	static String cookT(int h, int m, int cook) {
         // m + cook = 60 * (1 ~ 16)
         int hour = (m+cook)/60;
-		if(60*1 <= (m+cook) && (m+cook) <= 60*16) {
-			// h + ((m+cook)/60) >= 24 -> h -= 24 
-			if( h + hour >= 24) {
-				h -= 24;
-			}
-			switch(hour) {
-				case 1: h += 1; break;
-				case 2: h += 2; break;
-				case 3: h += 3; break;
-				case 4: h += 4; break;
-				case 5: h += 5; break;
-				case 6: h += 6; break;
-				case 7: h += 7; break;
-				case 8: h += 8; break;
-				case 9: h += 9; break;
-				case 10: h += 10; break;
-				case 11: h += 11; break;
-				case 12: h += 12; break;
-				case 13: h += 13; break;
-				case 14: h += 14; break;
-				case 15: h += 15; break;
-				case 16: h += 16; break;
-			}
-			m = (m+cook) % 60;
-		} else if((m+cook) > 60*16) {
-            if(h+hour >= 24) {
-                h -= 24;
-            }
-            h += hour;
-            m = (m+cook) % 60;
-        }
-        else {
-			m = m + cook;
+	if(60 <= (m+cook)) {
+		// h + ((m+cook)/60) >= 24 -> h -= 24 
+		if( h + hour >= 24) {
+			h -= 24;
 		}
+		h += hour;
+		m = (m+cook) % 60;
+	} else {
+		m = m + cook;
+	}
 		
-		return h + " " + m;
+	return h + " " + m;
 		
 	} //cookT
 	
