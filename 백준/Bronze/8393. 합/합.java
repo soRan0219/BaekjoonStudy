a79f4bd0a040d411174b20809cd184a8e88d6362
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2023-02-20 17:19:39 +0900

    [Bronze V] Title: 합, Time: 212 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum = 0;
+        for(int i=1; i<=n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
