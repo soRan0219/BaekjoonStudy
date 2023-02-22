7128aec9e2a4b70299af41ff5aabc672db7c8a28
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2023-02-22 15:32:18 +0900

    [Bronze V] Title: A+B - 8, Time: 172 ms, Memory: 16432 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testCase = Integer.parseInt(br.readLine());
+		
+		int[] sumArr = new int[testCase];
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i=0; i<sumArr.length; i++) {
+			String s = br.readLine();
+			StringTokenizer st = new StringTokenizer(s);
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			
+			sumArr[i] = num1 + num2;
+			bw.write("Case #" + (i+1) + ": " + num1 + " + " + num2 + " = " + sumArr[i]);
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
+		
+
+ 	} //main
+}
