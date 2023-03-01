8fe216728b07728008dc1bb59383c15dea1a845f
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2023-03-01 14:34:32 +0900

    [Bronze V] Title: X보다 작은 수, Time: 652 ms, Memory: 30696 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int a : arr) {
+			if(a < x) {
+				System.out.print(a + " ");
+			}
+		}
+
+	} //main
+}
