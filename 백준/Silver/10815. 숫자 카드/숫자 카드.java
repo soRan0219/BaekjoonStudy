718856e9061c7fc04aaab252d16b8bf5bf69222a
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2024-01-14 14:03:18 +0900

    [Silver V] Title: 숫자 카드, Time: 1236 ms, Memory: 123024 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,63 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[] cards;
+    static StringBuilder sb = new StringBuilder();
 	
-	//찾는 값보다 같거나 큰 첫번째 요소 인덱스
-	static int lowerBound(List<Integer> list, int key) {
+	static void matchNum(int num) {
+		int result = 0;
+		
 		int pl = 0;
-		int pr = list.size();
+		int pr = cards.length-1;
 		
-		while(pl < pr) {
+		while(pl <= pr) {
 			int pc = (pl+pr) / 2;
-			
-			if(list.get(pc) >= key) pr = pc;
-			else pl = pc +1;
+			if(cards[pc] > num) {
+				pr = pc-1;
+			} else if(cards[pc] <= num) {
+				pl = pc + 1;
+				if(cards[pc] == num) {
+					result = 1;
+					break;
+				}
+			}
 		}
-		
-		return pr;
-	}
-	
-	//찾는 값보다 큰 첫번째 요소 인덱스
-	static int upperBound(List<Integer> list, int key) {
-		int pl = 0;
-		int pr = list.size();
-		
-		while(pl < pr) {
-			int pc = (pl+pr) / 2;
-			
-			if(list.get(pc) <= key) pl = pc + 1;
-			else pr = pc;
-		}
-		
-		return pr;
+		sb.append(result + " ");
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		cards = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		List<Integer> cards = new ArrayList<>();
 		
 		for(int i=0; i<n; i++) {
-			cards.add(Integer.parseInt(st.nextToken()));
+			cards[i] = Integer.parseInt(st.nextToken());
 		}
-		Collections.sort(cards);
-		
+		Arrays.sort(cards);
+        
 		int m = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<m; i++) {
-			int key = Integer.parseInt(st.nextToken());
-			int isExist = upperBound(cards, key)-lowerBound(cards, key)==0 ? 0 : 1;
-			sb.append(isExist + " ");
-		}
 		
+		for(int i=0; i<m; i++) {
+			matchNum(Integer.parseInt(st.nextToken()));
+		}
 		System.out.println(sb);
 	}
 }
