ae94d627f0b0180dabc22c9628d3309d93799b8f
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2024-01-26 19:32:52 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 692 ms, Memory: 75160 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, K;
+	static int cnt;
+    static int[] sorted;
+	
+	static void mergeSort(int[] arr, int left, int right) {
+		if(left < right) {
+			int mid = (left+right) / 2;
+			mergeSort(arr, left, mid);
+			mergeSort(arr, mid+1, right);
+			merge(arr, left, mid, right);
+		}
+	}
+	
+	//부분 리스트 병합하여 전체 리스트를 정렬된 상태로 만들기
+	//부분 리스트들은 이미 정렬된 상태
+	static void merge(int[] arr, int left, int mid, int right) {
+		int i = left;
+		int j = mid + 1;
+		int t = 0;
+		
+		while(i<=mid && j<=right) {
+			if(arr[i] <= arr[j]) {
+				if(++cnt == K) {
+					System.out.println(arr[i]);
+					return;
+				}
+				sorted[t++] = arr[i++];
+			} else {
+				if(++cnt == K) {
+					System.out.println(arr[j]);
+					return;
+				}
+				sorted[t++] = arr[j++];
+			}
+		}
+		
+		//왼쪽 배열 부분이 남은 경우
+		while(i <= mid) {
+			if(++cnt == K) {
+				System.out.println(arr[i]);
+				return;
+			}
+			sorted[t++] = arr[i++];
+		}
+		//오른쪽 배열 부분이 남은 경우
+		while(j <= right) {
+			if(++cnt == K) {
+				System.out.println(arr[j]);
+				return;
+			}
+			sorted[t++] = arr[j++];
+		}
+		
+		//결과를 arr에 저장
+		i = left;
+		t = 0;
+		while(i <= right) arr[i++] = sorted[t++];
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		sorted = new int[N];
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		mergeSort(arr, 0, N-1);
+		if(cnt < K) System.out.println(-1);
+	}
+}
