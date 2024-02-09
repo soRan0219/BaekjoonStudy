09e1982743978339cfe8cf4817ed29113a8eae9e
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2024-02-09 18:58:33 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,39 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.EmptyStackException;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static String isVPS(String par) {
-		Stack<Character> pStack = new Stack<>();
+	static String isVPS(String gwal) {
+		Stack<Character> stack = new Stack<>();
 		
-		for(int i=0; i<par.length(); i++) {
-			if(par.charAt(i) == '(') pStack.push(par.charAt(i));
-			else if(par.charAt(i) == ')') {
-				try{
-					pStack.pop();
+		for(int i=0; i<gwal.length(); i++) {
+			if(gwal.charAt(i) == '(') {
+				stack.push(gwal.charAt(i));
+			} else if(gwal.charAt(i) == ')') {
+				try {
+					stack.pop();
 				} catch(EmptyStackException e) {
-					return "NO\n";
+					return "NO";
 				}
 			}
 		}
-		if(pStack.isEmpty()) return "YES\n";
-		else return "NO\n";
 		
-	} //isVPS()
+		if(!stack.isEmpty()) return "NO";
+		else return "YES";
+		
+	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
 		
-		for(int i=0; i<t; i++)
-			sb.append(isVPS(br.readLine()));
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<t; i++) {
+			sb.append(isVPS(br.readLine()) + "\n");
+		}
 		
 		System.out.println(sb);
-		
 	} //main()
 
 }
