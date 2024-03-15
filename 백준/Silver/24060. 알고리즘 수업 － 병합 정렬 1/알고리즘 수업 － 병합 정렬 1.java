112c04cea72bdf2c39f331707f503165e2394b39
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2024-03-15 19:05:10 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 660 ms, Memory: 74384 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -1,12 +1,12 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
+//병합정렬
 public class Main {
 	static int N, K;
 	static int cnt;
-	static int[] sorted;
-	static StringBuilder sb;
+    static int[] tmp;
+//	static StringBuilder sb = new StringBuilder();
 	
 	static void mergeSort(int[] arr, int left, int right) {
 		if(left < right) {
@@ -17,43 +17,32 @@ public class Main {
 		}
 	}
 	
-	//부분 리스트 병합하여 전체 리스트를 정렬된 상태로 만들기
-	//부분 리스트들은 이미 정렬된 상태
+	//arr[left..mid]와 arr[mid+1..right]을 병합하여 arr[left..right]를 정렬
 	static void merge(int[] arr, int left, int mid, int right) {
 		int i = left;
 		int j = mid + 1;
 		int t = 0;
 		
 		while(i<=mid && j<=right) {
-			if(arr[i] <= arr[j]) {
-				sorted[t++] = arr[i++];
-			} else {
-				sorted[t++] = arr[j++];
-			}
+			if(arr[i] <= arr[j]) tmp[t++] = arr[i++];
+			else tmp[t++] = arr[j++];
 		}
+		//왼쪽 부분이 남은 경우
+		while(i <= mid) tmp[t++] = arr[i++];
+		//오른쪽 부분이 남은 경우
+		while(j <= right) tmp[t++] = arr[j++];
 		
-		//왼쪽 배열 부분이 남은 경우
-		while(i <= mid) {		
-			sorted[t++] = arr[i++];
-		}
-		//오른쪽 배열 부분이 남은 경우
-		while(j <= right) {
-            sorted[t++] = arr[j++];
-        }
-		
-		//결과를 arr에 저장
+		//정렬한 결과를 arr에 복사
 		i = left;
 		t = 0;
 		while(i <= right) {
-			//24060
+			arr[i++] = tmp[t++];
 			if(++cnt == K) {
-				System.out.println(sorted[t]);
-				return;
-			}
-			arr[i++] = sorted[t++];
-			//24061
-//			if(++cnt==K) {
-//				for(int a : arr)
+                System.out.println(arr[i-1]);
+            }
+			//BOJ24061
+//			if(++cnt == K) {
+//				for(int a : arr) 
 //					sb.append(a + " ");
 //				System.out.println(sb);
 //			}
@@ -63,18 +52,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
+		int[] a = new int[N];
+        tmp = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
-		sorted = Arrays.copyOf(arr, N);
+			a[i] = Integer.parseInt(st.nextToken());
 		
-		mergeSort(arr, 0, N-1);
+		mergeSort(a, 0, N-1);
 		if(cnt < K) System.out.println(-1);
 	}
 }
