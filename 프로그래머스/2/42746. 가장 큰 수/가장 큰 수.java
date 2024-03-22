1077673ccd8a06fe836ee2227132eef990a78c60
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2024-03-22 19:24:27 +0900

    [level 2] Title: 가장 큰 수, Time: 9.07 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+		StringBuilder answer = new StringBuilder();
+		
+		String[] strArr = new String[numbers.length];
+		for(int i=0; i<numbers.length; i++) {
+			strArr[i] = numbers[i] + "";
+		}
+		
+		Arrays.sort(strArr, (s1, s2) -> (s2+s1).compareTo(s1+s2));
+		
+		for(String s : strArr)
+			answer.append(s);
+		
+        if(answer.charAt(0) == '0') return "0";
+        
+		return answer.toString();
+    }
+}
