35c1c868e696a9425a649f089825ed7b541adac5
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2024-04-06 15:19:21 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,28 +2,24 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] citations) {
+        int n = citations.length;
         Arrays.sort(citations);
         
-        int n = citations.length;
-        
         int h_idx = 0;
+        
         for(int i=0; i<=citations[n-1]; i++) {
-        	int cnt = 0;
+        	
         	for(int j=0; j<n; j++) {
-        		if(citations[j] >= i) {
-        			cnt = n - j;
-        			break;
-        		}
-        	}
-        	if(cnt >= i) {
-        		boolean flag = true;
-        		for(int j=0; j<n-cnt; j++) {
-        			if(citations[j] > i) {
-        				flag = false;
+        		if(citations[j] >= i && n-j >= i) {
+        			if(j>0 && citations[j-1] <= i) {
+        				h_idx = i;
         				break;
-        			}
+        			} else if(j == 0) {
+                        h_idx = i;
+                        break;
+                    }
         		}
-        		if(flag) h_idx = i;
+        	
         	}
         }
         
