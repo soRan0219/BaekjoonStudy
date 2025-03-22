dbf6bdf18f0e28733a8927d47556cff9550bc1bc
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2025-03-22 17:17:11 +0900

    [level 1] Title: 모의고사, Time: 3.09 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -8,14 +8,12 @@ class Solution {
 		int[] supo_2 = {2, 1, 2, 3, 2, 4, 2, 5};
 		int[] supo_3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 		
-		int i = 0;
 		int[] score = {0, 0, 0};
 		
-		while(i != answers.length) {
+		for(int i=0; i<answers.length; i++) {
 			if(answers[i] == supo_1[i%5]) score[0]++;
 			if(answers[i] == supo_2[i%8]) score[1]++;
 			if(answers[i] == supo_3[i%10]) score[2]++;
-			i++;
 		}
 		
 		int max = Math.max(score[0], Math.max(score[1], score[2]));
