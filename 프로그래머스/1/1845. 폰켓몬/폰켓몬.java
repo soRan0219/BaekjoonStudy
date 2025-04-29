f6de84be9e9b8818978be5db28346eb1136eadfe
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2025-04-29 14:43:55 +0900

    [level 1] Title: 폰켓몬, Time: 6.72 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-    	int mons = nums.length / 2;
-    	Map<Integer, Integer> map = new HashMap<>();
+    	Set<Integer> pokemons = new HashSet<>();
     	
-        for(int i=0; i<nums.length; i++) {
-        	if(map.containsKey(nums[i])) map.put(nums[i], map.get(nums[i])+1);
-        	else map.put(nums[i], 1);
-        }
-        
-        return Math.min(mons, map.size());
+    	for(int n : nums) pokemons.add(n);
+    	
+    	if(nums.length/2 > pokemons.size()) return pokemons.size();
+    	else return nums.length/2;
     }
 }
