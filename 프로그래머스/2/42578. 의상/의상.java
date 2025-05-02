8175a75bd973ad085303b16d1474518e2dcad1d4
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2025-05-02 18:22:46 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,20 +4,18 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         
+        //의상 개수만 쓰므로 숫자로 저장
         Map<String, Integer> closet = new HashMap<>();
         
-        for(int i=0; i<clothes.length; i++) {
-        	if(!closet.containsKey(clothes[i][1])) closet.put(clothes[i][1], 0);
-        	closet.put(clothes[i][1], closet.get(clothes[i][1])+1);
+        for(String[] c : clothes) {
+        	int num = closet.containsKey(c[1]) ? closet.get(c[1]) : 0;
+        	closet.put(c[1], ++num);
         }
         
-        Set<String> set = closet.keySet();
-        Iterator<String> it = set.iterator();
+        Set<String> keys = closet.keySet();
+        Iterator<String> it = keys.iterator();
+        while(it.hasNext()) answer *= closet.get(it.next()) + 1;
         
-        while(it.hasNext()) {
-        	answer *= closet.get(it.next()) + 1;
-        }
-        
-        return answer - 1;
+        return answer -1;
     }
 }
