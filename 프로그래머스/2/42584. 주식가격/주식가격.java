128bad61287a50938db1e892cecc5a1c96b85f88
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2025-06-18 14:41:06 +0900

    [level 2] Title: 주식가격, Time: 28.40 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,22 +2,11 @@ class Solution {
     public int[] solution(int[] prices) {
 		int[] answer = new int[prices.length];
 		
-		int notDrop = 1;
-		
 		for(int i=0; i<prices.length; i++) {
-			
 			for(int j=i+1; j<prices.length; j++) {
-				
-				if(j==prices.length-1 || prices[i]>prices[j]) {
-					answer[i] = notDrop;
-					notDrop = 1;
-					break;
-				} else if(prices[i] <= prices[j]) {
-					notDrop++;
-				}
-				
+                answer[i]++;
+                if(j==prices.length-1 || prices[j]<prices[i]) break;
 			}
-			
 		}
 		
 		return answer;
