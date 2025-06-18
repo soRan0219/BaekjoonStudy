f020651d627dee10396ffed06e176c2703fb7aa5
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2025-06-18 18:07:28 +0900

    [level 2] Title: 더 맵게, Time: 1605.42 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,11 +3,9 @@ import java.util.PriorityQueue;
 class Solution {
     public int solution(int[] scoville, int k) {
         int answer = 0;
-        
-        if(scoville.length<2 && scoville[scoville.length-1]<k) return -1; 
-        
+                
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int i=0; i<scoville.length; i++) minHeap.offer(scoville[i]);
+        for(int s : scoville) minHeap.offer(s);
         
         while(minHeap.size()>=2 && k>minHeap.peek()) {
             int min = minHeap.poll();
