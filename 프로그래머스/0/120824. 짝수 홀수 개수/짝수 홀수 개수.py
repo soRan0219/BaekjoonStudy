c3b072d762a876ed1d2146c426935abd8b037831
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2025-07-21 20:11:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    odd = 0
+    even = 0
+    for i in num_list:
+        if i%2 == 0:
+            even += 1
+        else: 
+            odd += 1
+    answer = [even, odd]
+    return answer
