0d7761d88a929171e455afbf41ac6f7151a95760
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2026-03-08 14:37:39 +0900

    Update 2차원으로 만들기.go

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.go
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.go
@@ -14,3 +14,15 @@ func solution(num_list []int, n int) [][]int {
     }
     return result
 }
+
+// 개선
+func solution2(num_list []int, n int) [][]int {
+    var result [][]int
+    
+    // n개씩 건너뛰면서
+    for i:=0; i<len(num_list); i+=n {
+        // 슬라이싱으로 필요한 만큼 한번에 잘라넣기
+        result = append(result, num_list[i:i+n])
+    }
+    return result
+}
