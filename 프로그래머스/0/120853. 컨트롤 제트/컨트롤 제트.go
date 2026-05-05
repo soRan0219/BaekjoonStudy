442d4a65d74ba08f6c5a67922c7c1568009e3bd8
soRan0219 <124494718+soRan0219@users.noreply.github.com>
2026-05-05 14:06:16 +0900

    Update 컨트롤 제트.go

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.go
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.go
@@ -6,17 +6,19 @@ import (
 func solution(s string) int {
     answer := 0
     arr := strings.Fields(s)
+    var stack []int
     
-    var prev int
     for _, str := range arr {
-        num, err := strconv.Atoi(str)
-        
-        if err == nil {
-            prev = num
-            answer += num
+        if str != "Z" {
+            if num, err := strconv.Atoi(str); err == nil {
+                answer += num
+                stack = append(stack, num)
+            }
         } else {
-            if str == "Z" {
-                answer -= prev
+            if len(stack) > 0 {
+                lastIdx := len(stack) - 1
+                answer -= stack[lastIdx]
+                stack = stack[:lastIdx]
             }
         }
     }
